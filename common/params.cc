61cc5e0b28befed0c904ed119ab5d3b2ef6c7227
FrogAi <91348155+FrogAi@users.noreply.github.com>
2025-02-12 14:12:27 -0700

    February 15th, 2025 Patch

--- common/params.cc
+++ common/params.cc
@@ -76,7 +76,7 @@ std::string ensure_params_path(const std::string &prefix, const std::string &pat
 class FileLock {
 public:
   FileLock(const std::string &fn) {
-    if (fn.rfind("/cache", 0) == 0) {
+    if (fn.rfind("/cache", 0) == 0 || fn.rfind("/data/params_backup", 0) == 0) {
       return;
     }
 
@@ -492,7 +492,6 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"SLCPriority1", PERSISTENT | FROGPILOT_CONTROLS},
     {"SLCPriority2", PERSISTENT | FROGPILOT_CONTROLS},
     {"SLCPriority3", PERSISTENT | FROGPILOT_CONTROLS},
-    {"SmartTurnControl", PERSISTENT | FROGPILOT_CONTROLS},
     {"SNGHack", PERSISTENT | FROGPILOT_VEHICLES},
     {"SoundToDownload", CLEAR_ON_MANAGER_START},
     {"SpeedLimitAccepted", CLEAR_ON_MANAGER_START},
